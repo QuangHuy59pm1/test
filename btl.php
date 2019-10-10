f795fd7ed5ae04f12d76cfe843fe91e0df60072c
huynq721 <huynq721@wru.vn>
2019-10-10 15:31:26 +0700

    first commit

---
+++ btl.php
@@ -0,0 +1,167 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="css/bootstrap.css">
+  <link rel="stylesheet" href="css/style.css">
+
+  <title>Trang chủ</title>
+</head>
+<body>
+  <div class="body">
+  <?php include "header.php"; ?>
+    <div class="container">
+      <div class="mb1">
+        <div class="row">
+          <div class="col-md-9">
+            <a class="td" href="" style="font-size: 30px "> <u style="color:rgb(231, 172, 84)"> Tin tức </u> </a>
+            <div class="row">
+              <div class="col-md-4">
+                <img src="img/img1.jpg">
+                <a href=""> <b> NTTU - Hòa chung không khí vui tươi của các bạn thiếu nhi trong cả nước sắp đón một
+                    mùa Trung thu mới,..</b>
+                </a>
+                <p>Tham gia ngày hội trăng rằm, ngoài được xem hát múa, diễn văn nghệ, vui chơi cùng chị Hằng, chú
+                  Cuội các bé ,..</p>
+              </div>
+              <div class="col-md-4">
+                <img src="img/img2.png">
+                <a href=""><b>Tân sinh viên - Bí quyết để tuổi 20 của bạn trở thành mốc thời gian đáng nhớ nhất</b></a>
+                <p>Tuổi 20 là khoảng thời gian “rắc rối” nhất của con người. 20 tuổi, bạn không còn là một đứa trẻ vô âu
+                  vô lo, nhưng cũng chẳng phải là một người lớn thực thụ. Người ta nói tuổi 20 “dở dở ương ương”, bạn
+                  chẳng biết được mình nên làm gì và phải làm như thế nào. </p>
+              </div>
+              <div class="col-md-4">
+                <img src="img/img3.png">
+                <a href=""><b>ĐH Nguyễn Tất Thành nhận hồ sơ xét tuyển học bạ đợt cuối đến ngày 03/09/2019</b> </a>
+                <p>NTTU - Với ưu điểm linh hoạt, thuận tiện và đặc biệt là cho phép thí sinh (TS) chủ động hơn trong quá
+                  trình xét tuyển, phương thức xét tuyển bằng học bạ lớp 12 đang dần khẳng định được sức hút của mình.
+                </p>
+              </div>
+            </div>
+
+          </div>
+
+
+          <div class="col-md-3">
+            <a class="td" href="" style="font-size: 30px"> <u style="color:rgb(231, 172, 84)">Media </u> </a>
+            <iframe width="270" height="140" src="https://www.youtube.com/embed/iZ1EUr2DAMs" frameborder="0"
+              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <ul>
+              <li><a href=""> Nguyễn Tất Thành - 20 năm 1 trặng đường</a></li>
+              <li><a href=""> Nguyễn Tất Thành - 20 năm 1 trặng đường</a></li>
+              <li><a href=""> Nguyễn Tất Thành - 20 năm 1 trặng đường</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      <div class="mb2">
+        <div class="row">
+          <div class="col-md-4">
+            <a class="td" href="" style="font-size: 30px"> <u style="color:rgb(231, 172, 84)"> Hợp tác quốc tế </u> </a>
+            <img src="img/img4.jpg" alt="">
+            <a href=""><b>Sinh viên ĐH Nguyễn Tất Thành hào hứng tham gia giao lưu văn hóa cùng sinh viên ĐH Gakushuin
+                (Nhật Bản)</b></a>
+            <p>NTTU – Sáng ngày 09/09/2019, tại cơ sở An Phú Đông, Trường ĐH Nguyễn Tất Thành đã tổ chức chương trình
+              giao lưu văn hóa với sinh viên Trường ĐH Gakushuin (Nhật Bản)</p>
+
+          </div>
+          <div class="col-md-4">
+            <a class="td" href="" style="font-size: 30px"> <u style="color:rgb(231, 172, 84)"> Môi trường hoạt động </u>
+            </a>
+            <img src="img/img4.jpg" alt="">
+            <a href=""><b>Sinh viên ĐH Nguyễn Tất Thành hào hứng tham gia giao lưu văn hóa cùng sinh viên ĐH Gakushuin
+                (Nhật Bản)</b></a>
+            <p>NTTU – Sáng ngày 09/09/2019, tại cơ sở An Phú Đông, Trường ĐH Nguyễn Tất Thành đã tổ chức chương trình
+              giao lưu văn hóa với sinh viên Trường ĐH Gakushuin (Nhật Bản)</p>
+
+          </div>
+          <div class="col-md-4">
+            <a class="td" href="" style="font-size: 30px"> <u style="color:rgb(231, 172, 84)"> Sự Kiện nổi bật </u> </a>
+            
+            <ul class="list-unstyled">
+              <li class="media event">
+<div class="d-flex align-items-center mr-3 thumb c-white">
+<span class="inner text-center w-100">
+Sep<br>
+<b>14</b>
+</span>
+</div>
+<div class="media-body">
+<h5 class="title mt-0"><a href="/web/su-kien/le-ky-ket-hop-tac-voi-tap-chi-dan-chu-phap-luat">Lễ ký kết hợp tác với Tạp chí Dân chủ &amp; Pháp luật</a></h5>
+<ul class="breadcrumb mb-0 bgm-white">
+<li class="breadcrumb-item col-xs-auto time">
+<i class="fa fa-clock-o mr-2" aria-hidden="true"></i>08:00
+</li>
+<li class="breadcrumb-item col-xs-auto">Phòng họp 1 - Cơ sở An Phú Đông</li>
+</ul>
+</div>
+</li>
+              <li class="media event">
+<div class="d-flex align-items-center mr-3 thumb c-white">
+<span class="inner text-center w-100">
+Sep<br>
+<b>14</b>
+</span>
+</div>
+<div class="media-body">
+<h5 class="title mt-0"><a href="/web/su-kien/hop-mat-giang-vien-thuc-hanh-khoa-luat">Họp mặt giảng viên thực hành Khoa Luật</a></h5>
+<ul class="breadcrumb mb-0 bgm-white">
+<li class="breadcrumb-item col-xs-auto time">
+<i class="fa fa-clock-o mr-2" aria-hidden="true"></i>08:00
+</li>
+<li class="breadcrumb-item col-xs-auto">Phòng họp 1 - Cơ sở An Phú Đông</li>
+</ul>
+</div>
+</li>
+              <li class="media event">
+<div class="d-flex align-items-center mr-3 thumb c-white">
+<span class="inner text-center w-100">
+Sep<br>
+<b>11</b>
+</span>
+</div>
+<div class="media-body">
+<h5 class="title mt-0"><a href="/web/su-kien/chuong-trinh-dong-hanh-cung-suc-khoe-hoc-duong-tu-van-cham-soc-dieu-tri-cac-ben-ve-da-nam-2019">Chương trình "Đồng hành cùng sức khỏe học đường: Tư vấn, chăm sóc, điều trị các bện về da" năm 2019</a></h5>
+<ul class="breadcrumb mb-0 bgm-white">
+<li class="breadcrumb-item col-xs-auto time">
+<i class="fa fa-clock-o mr-2" aria-hidden="true"></i>07:30
+</li>
+<li class="breadcrumb-item col-xs-auto">Hội trường A.801, 300A Nguyễn Tất Thành, phường 13, quận 4, TP. HCM</li>
+</ul>
+</div>
+</li>
+      </ul>
+ </div>
+
+        </div>
+      </div>
+      <?php include "footer.php"; ?>
+
+    </div>
+
+  </div>
+
+
+
+<table style=" border: 1px solid red ">
+<?php
+      include('getCustomer.php');
+  ?>
+
+
+</table>
+ 
+
+
+
+  </div>
+  <script src="js/jquery-3.4.1.js"></script>
+  <script src="js/bootstrap.js"></script>
+</body>
+
+</html>
